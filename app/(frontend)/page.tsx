f0336029e82c712dc32e93a75020e7a295d5cd45
seigiakatsuka <minuitspence@gmail.com>
2026-04-01 14:28:37 -0400

    Updated link to return to midnight-code.tech

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -4,8 +4,9 @@ export default async function Page() {
   return (
     <div>
       <h1 className="text-4xl font-bold">Late Night Code</h1>
-        <hr/>
-        <Link href="/posts">View posts &rarr;</Link>
+      <hr />
+      <Link href="https://midnight-code.tech">Back to Home</Link>
+      <Link href="/posts">View posts &rarr;</Link>
     </div>
-  )
+  );
 }
